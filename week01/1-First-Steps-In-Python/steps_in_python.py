e1d3819ea1dccfa83133b07172ceb354ae4bf30a
Rositsazz <ross_zz@mail.bg>
2016-10-17 14:58:19 +0300

    Template for the first python tasks

---
+++ week01/1-First-Steps-In-Python/steps_in_python.py
@@ -0,0 +1,33 @@
+# A function for counting the digits of a number
+def count_digits(number):
+    pass
+
+
+# Create list with the digits of a number
+def to_digits(number):
+    pass
+
+
+# Create number from array
+def to_number(digits):
+    pass
+
+
+# Count the vowels in a string
+def count_vowels(string):
+    pass
+
+
+# Check in a given number is prime
+def prime_number(number):
+    pass
+
+
+# fibonacci sequince
+def fibonacci(number):
+    pass
+
+
+# Check if a given string is palindrome
+def palindrome(string):
+    pass
